205c32170941626b36e3171e86923f03f63d7f7c
m2groene@uwaterloo.ca <m2groene@uwaterloo.ca>
2024-10-18 11:38:24 +0200

    ready for testing with photoresistor

--- main/main.ino
+++ main/main.ino
@@ -9,9 +9,11 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   
-  analogWrite(13, 128);
+  digitalWrite(13,HIGH);
   
   float analogValue = analogRead(A0);  // Read the analog input from pin A0
   Serial.println(analogValue);   // Print the value to the serial monitor
   delay(500);  // Delay for readability (0.5 seconds)
+
+  
 }
